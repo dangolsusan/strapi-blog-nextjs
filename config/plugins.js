ca39c01ab9921bc31459bb314b7af62a6493f44a
dangolsusan <dangol.susan@webexpertsnepal.com>
2024-07-16 14:04:56 +0545

    crypto added

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,11 @@
+const crypto = require("crypto");
 module.exports = ({ env }) => ({
   // ...
   "users-permissions": {
     config: {
       jwt: {
         expiresIn: "7d",
-        jwtSecret: env("JWT_SECRET", "susandangol"),
+        jwtSecret: env("JWT_SECRET", crypto.randomBytes(16).toString("base64")),
       },
     },
   },
